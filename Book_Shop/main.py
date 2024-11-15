a368205ce2b7a5226e8d4026b41fa52bf1f7312d
Pokimonka <alenkaponomareva7@gmail.com>
2024-11-15 23:52:33 +0300

    Book shop1

--- Book_Shop/main.py
+++ Book_Shop/main.py
@@ -2,30 +2,48 @@ import json
 import os
 
 import sqlalchemy
-from sqlalchemy import false, func
 from sqlalchemy.orm import sessionmaker
 
 from model import create_tables, Publisher, Shop, Book, Stock, Sale
 
+# os.environ["DB_USER_NAME"] = "postgres"
+# os.environ["PASSWORD"] = "password"
+# os.environ["DB_NAME"] = "book_shop"
+
+DB_USER_NAME = os.getenv("DB_USER_NAME")
+PASSWORD = os.getenv("PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+DSN = f'postgresql://{DB_USER_NAME}:{PASSWORD}@localhost:5432/{DB_NAME}'
+engine = sqlalchemy.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def get_shops(id_or_name):
+     response = (session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)
+                    .select_from(Shop)
+                    .join(Stock).filter(Stock.id_shop == Shop.id)
+                    .join(Book).filter(Book.id == Stock.id_book)
+                    .join(Publisher).filter(Publisher.id == Book.id_publisher)
+                    .join(Sale).filter(Sale.id_stock == Stock.id))
+     if id_or_name.isdigit():
+          res = response.filter(Publisher.id == id_or_name).all()
+     else:
+          res = response.filter(Publisher.name == id_or_name).all()
+     if res:
+          for title, name, price, date in res:
+               #если оставить ваше форматирование, то в ответе много пробелов
+               #Modern Operating Systems                 | OZON       | 16.00    | 25-10-2018
+               #print(f"{title: <40} | {name: <10} | {price: <8} | {date.strftime('%d-%m-%Y')}")
+               print(f"{title} | {name} | {price} | {date.strftime('%d-%m-%Y')}")
+     else:
+          print('Такой покупки не найдено')
+
 
 if __name__ == '__main__':
-
-     #для тестирования
-     # os.environ["DB_USER_NAME"] = "postgres"
-     # os.environ["PASSWORD"] = "password"
-     # os.environ["DB_NAME"] = "book_shop"
-
-     DB_USER_NAME = os.getenv("DB_USER_NAME")
-     PASSWORD = os.getenv("PASSWORD")
-     DB_NAME = os.getenv("DB_NAME")
-
-     DSN = f'postgresql://{DB_USER_NAME}:{PASSWORD}@localhost:5432/{DB_NAME}'
-     engine = sqlalchemy.create_engine(DSN)
-
-     create_tables(engine)
-
-     Session = sessionmaker(bind=engine)
-     session = Session()
 
      with open("fixtures/test_data.json", 'r') as td:
           data = json.load(td)
@@ -41,18 +59,9 @@ if __name__ == '__main__':
           session.add(model(id=record.get('pk'), **record.get('fields')))
      session.commit()
 
-     print("Введите имя автора:")
+     print("Введите имя или ID автора:")
      publisher_name = input()
 
-     response = (session.query(Book.title, Shop.name, Sale.price, func.to_char(Sale.date_sale, 'dd-mm-yyyy'))
-          .join(Stock.book).filter(Stock.id_book == Book.id)
-          .join(Shop).filter(Shop.id == Stock.id_shop)
-          .join(Publisher).filter(Publisher.id == Book.id_publisher).filter(Publisher.name == publisher_name)
-          .join(Sale).filter(Sale.id_stock == Stock.id)).all()
-     if response:
-          for item in response:
-               print(' | '.join(list(item)))
-     else:
-          print('Такой покупки не найдено')
+     get_shops(publisher_name)
 
      session.close()
